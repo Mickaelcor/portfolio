1ad40317b765c871403bccda5a0398d13a20e4e2
Mickael.C <mickael.correia@lagrandeclasse.fr>
2023-03-10 13:08:41 +0100

    minor modification

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
     }
 
     return (
-        <div name="acceuil" className='w-full bg-gradient-to-b from-black via-black to-gray-800 pt-52 pb-60'>
+        <div name="acceuil" className='w-full bg-gradient-to-b from-black via-black to-gray-800 pt-52 pb-60 2xl:pb-96'>
             <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
                 <div className='flex flex-col justify-center h-full'>
                     <h2 className='text-4xl sm:text-7xl font-bold text-white'>Développeur web junior</h2>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -32,7 +32,7 @@ const NavBar = () => {
     return (
         <div className='flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed'>
             <div>
-                <h1 className='text-5xl font-signature ml-2'>Mickael Correia</h1>
+                <h1 className='cursor-pointer text-5xl font-signature ml-2 hover:text-sky-500 hover:scale-110 duration-300'> <Link to="acceuil">Mickael Correia</Link></h1>
             </div>
 
             <ul className='hidden md:flex'>
